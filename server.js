e5843456c04c8bf6555a2ac7696c2c1465c7f2de
Akshay Bhaskar <akshay5060@gmail.com>
2017-08-20 00:35:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/Article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/Article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
