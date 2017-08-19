1709cba130d5e21bea1bd1e643c92363fd92b99c
Akshay Bhaskar <akshay5060@gmail.com>
2017-08-20 00:57:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
- 
-app.get('/Article-one', function (req, res)
-    {
-  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
-    });
 });
-
-
-
 
 app.get('/Article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
