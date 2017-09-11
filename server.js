b376ac1383407da35fcc1b22306f21d0d1b7bb13
Akshay Bhaskar <akshay5060@gmail.com>
2017-09-11 17:15:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = required('pg').pool;
+
+var config ={
+    user:'akshay5060',
+    database:'akshay5060',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -99,6 +108,18 @@ var htmlTemplate=`
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new pool(config);
+app.get('/test-db', function(req, res){
+   //make req and return response 
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/:ArticleName', function (req, res) {
